4e44d01d409ad432ebee227f2007b395eff13d3b
DeimisM <117855291+DeimisM@users.noreply.github.com>
2024-04-18 19:22:11 +0300

    updates

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,6 +6,10 @@ public class Player : MonoBehaviour
 {
     Rigidbody2D rb;
     Vector2 input;
+    public float moveSpeed = 20f;
+    public GameObject landParticles;
+    bool hasLanded;
+
 
     private void Start()
     {
@@ -15,11 +19,35 @@ public class Player : MonoBehaviour
     private void Update()
     {
         var newInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        if(newInput != Vector2.zero)
+
+        if(Mathf.Abs(newInput.x) > 0 && Mathf.Abs(newInput.y) > 0)
+        {
+            newInput.y = 0;
+        }
+
+        if(rb.velocity.magnitude < 0.1f && !hasLanded && newInput != input && newInput != Vector2.zero)
+        {
+            if (landParticles != null)
+            {
+                Instantiate(landParticles, transform.position, Quaternion.identity);
+            }
+             hasLanded = true;
+        }
+
+        if(newInput != Vector2.zero && rb.velocity.magnitude < 0.1f)
         {
             input = newInput;
             transform.up = -input;
+            hasLanded = false;
         }
-        rb.velocity = input * 5f;
+        rb.velocity = input * moveSpeed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Coin"))
+        {
+            Destroy(collision.gameObject);
+        }
     }
 }
